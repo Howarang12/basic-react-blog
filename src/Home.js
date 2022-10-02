58add37cc8cd63110e71f7f81fec6b7b97536561
Howard Wang <howard122696@gmail.com>
2022-10-01 21:43:49 -0700

    props

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react"
+import BlogList from "./BlogList";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {title: 'title 1', body: 'lorem ipsum...', author: 'mario', id: 1}, 
+    {title: 'title 2', body: 'lorem ipsum...', author: 'wario', id: 2}, 
+    {title: 'title 3', body: 'lorem ipsum...', author: 'dario', id: 3}, 
+  ])
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => (blog.id !== id))
+    setBlogs(newBlogs)
+  }
+
   return (  
     <div className="home">
-      <h2>Homepage</h2>
+      <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete}/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario') } title="Mario's Blogs" handleDelete={handleDelete}/>
     </div>
   );
 }
